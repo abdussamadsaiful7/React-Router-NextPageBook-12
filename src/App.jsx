c44390013a10fe41bd516dc800c5d535b64e3f9e
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-04-06 16:02:54 +0600

    added nab manu for mobile device

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import { Outlet, useLoaderData } from 'react-router-dom'
+import Header from './Components/Header/Header';
 
 function App() {
   const App = useLoaderData();
 
   return (
     <div className="App">
-     
+     <Header></Header>
      <Outlet/>
     </div>
   )
